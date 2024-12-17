6ce731d8f81e68e7d9517e1f9b8ba1968ef4ccf8
wooseok <plutoxxx1014@gmail.com>
2024-12-18 00:15:41 +0900

    feat: TodoRequest와 TodoResponse DTO 추가 및 필드 정리

--- src/main/java/com/ssary/diary_web/dto/TodoResponse.java
+++ src/main/java/com/ssary/diary_web/dto/TodoResponse.java
@@ -1,2 +1,15 @@
-package com.ssary.diary_web.dto;public class TodoResponse {
+package com.ssary.diary_web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+public class TodoResponse {
+    private Long id; // 할 일 ID
+    private String title; // 할 일 제목
+    private boolean completed; // 완료 상태
+    private LocalDate date; // 할 일 날짜
 }
