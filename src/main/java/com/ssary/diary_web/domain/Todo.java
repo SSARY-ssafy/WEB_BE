e90ccb57083d04096bf9a07d81c29739b1b8b54e
wooseok <plutoxxx1014@gmail.com>
2024-12-19 19:02:18 +0900

    데이터베이스의 todoãfeat: 테이블 스키마와 맞추기 위해 필드를 추가/수정

--- src/main/java/com/ssary/diary_web/domain/Todo.java
+++ src/main/java/com/ssary/diary_web/domain/Todo.java
@@ -16,7 +16,10 @@ public class Todo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private Long userId; // 유저 ID
     private String title; // 할 일 제목
+    private String content; // 할 일 상세 내용
     private boolean completed = false; // 기본 완료 상태는 false
-    private LocalDate date; // 할 일 날짜
+    private LocalDate start; // 시작 날짜
+    private LocalDate end; // 종료 날짜
 }
