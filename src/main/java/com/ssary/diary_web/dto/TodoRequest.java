2b0265cc4b7b7ac25cfcf07066230f6973e26ada
wooseok <plutoxxx1014@gmail.com>
2024-12-19 19:04:03 +0900

    데이터베이스의 todoTodoRequest와 Todofeat: Response를 데이터베이스 스키마와 일치하도록 수정

--- src/main/java/com/ssary/diary_web/dto/TodoRequest.java
+++ src/main/java/com/ssary/diary_web/dto/TodoRequest.java
@@ -8,6 +8,9 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class TodoRequest {
+    private Long userId; // 유저 ID
     private String title; // 할 일 제목
-    private LocalDate date; // 할 일 날짜
+    private String content; // 할 일 상세 내용
+    private LocalDate start; // 시작 날짜
+    private LocalDate end; // 종료 날짜
 }
--- src/main/java/com/ssary/diary_web/dto/TodoResponse.java
+++ src/main/java/com/ssary/diary_web/dto/TodoResponse.java
@@ -9,7 +9,10 @@ import java.time.LocalDate;
 @Builder
 public class TodoResponse {
     private Long id; // 할 일 ID
+    private Long userId; // 유저 ID
     private String title; // 할 일 제목
+    private String content; // 할 일 상세 내용
     private boolean completed; // 완료 상태
-    private LocalDate date; // 할 일 날짜
+    private LocalDate start; // 시작 날짜
+    private LocalDate end; // 종료 날짜
 }
